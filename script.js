a270a1caf2cd2738e6528f0b08d1a11ba70fc10a
ValdemirJu <val.demirpsj@gmail.com>
2020-12-02 23:19:34 -0300

    Implementa a chamada à api que retorna o valor

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
-let type = '';
-let brand = '';
-let model = '';
-let year = '';
+let apiType = '';
+let apiBrand = '';
+let apiModel = '';
+let apiYear = '';
 
-let marca = document.getElementById('marca');
-let modelo = document.getElementById('modelo');
-let ano = document.getElementById('ano');
-let combustivel = document.getElementById('combustivel');
-let valor = document.getElementById('valor');
+let infoBrand = document.getElementById('infoBrand');
+let infoModel = document.getElementById('infoModel');
+let infoYear = document.getElementById('infoYear');
+let infoFuel = document.getElementById('infoFuel');
+let infoValue = document.getElementById('infoValue');
 
-const typeSelector = document.getElementById('type');
-const brandSelector = document.getElementById('brand');
-const modelSelector = document.getElementById('model');
-const yearSelector = document.getElementById('year');
+let typeSelector = document.getElementById('apiType');
+let brandSelector = document.getElementById('apiBrand');
+let modelSelector = document.getElementById('apiModel');
+let yearSelector = document.getElementById('apiYear');
 
+function resetInfos() {
+	infoBrand.innerHTML = '<strong>Marca:</strong>';
+	infoModel.innerHTML = '<strong>Modelo:</strong>';
+	infoYear.innerHTML = '<strong>Ano:</strong>';
+	infoFuel.innerHTML = '<strong>Combustível:</strong>';
+	infoValue.innerHTML = '<strong>Valor:</strong>';
+}
 
 typeSelector.addEventListener('change', Event => {
 	brandSelector.innerHTML = '<option value="*">Selecionar</option>';
 	modelSelector.innerHTML = '<option value="*">Selecionar</option>';
 	yearSelector.innerHTML = '<option value="*">Selecionar</option>';
 	changeType();
+	resetInfos();
 });
 
 brandSelector.addEventListener('change', Event => {
 	modelSelector.innerHTML = '<option value="*">Selecionar</option>';
 	yearSelector.innerHTML = '<option value="*">Selecionar</option>';
 	changeBrand();
+	resetInfos();
 });
 
 modelSelector.addEventListener('change', Event => {
 	yearSelector.innerHTML = '<option value="*">Selecionar</option>';
 	changeModel();
+	resetInfos();
+});
+
+yearSelector.addEventListener('change', Event => {
+	changeYear();
+	resetInfos();
 });
 
 function changeType() {
 	let option = typeSelector.options[typeSelector.selectedIndex];
 
-	type = option.value;
-	updateBrands(type)
+	apiType = option.value;
+	updateBrands(apiType)
 }
 
 function updateBrands(type) {
 	if (type !== '*') {
-		fetch(`https://parallelum.com.br/fipe/api/v1/${type}/marcas`)
+		fetch(`https://parallelum.com.br/fipe/api/v1/${apiType}/marcas`)
 			.then(response => response.json())
 			.then(response => {
-				for (brand of response) {
+				for (let brand of response) {
 					brandSelector.innerHTML += `<option value="${brand.codigo}">${brand.nome}</option>`;
 				}
 			})
@@ -55,16 +70,16 @@ function updateBrands(type) {
 function changeBrand() {
 	let option = brandSelector.options[brandSelector.selectedIndex];
 
-	brand = option.value;
-	updateModels(brand)
+	apiBrand = option.value;
+	updateModels(apiBrand)
 }
 
 function updateModels(brand) {
 	if (brand !== '*') {
-		fetch(`https://parallelum.com.br/fipe/api/v1/${type}/marcas/${brand}/modelos`)
+		fetch(`https://parallelum.com.br/fipe/api/v1/${apiType}/marcas/${apiBrand}/modelos`)
 			.then(response => response.json())
 			.then(({ modelos }) => {
-				for (model of modelos) {
+				for (let model of modelos) {
 					modelSelector.innerHTML += `<option value="${model.codigo}">${model.nome}</option>`;
 				}
 			})
@@ -74,18 +89,40 @@ function updateModels(brand) {
 function changeModel() {
 	let option = modelSelector.options[modelSelector.selectedIndex];
 
-	model = option.value;
-	updateYear(model)
+	apiModel = option.value;
+	updateYear(apiModel)
 }
 
 function updateYear(model) {
 	if (model !== '*') {
-		fetch(`https://parallelum.com.br/fipe/api/v1/${type}/marcas/${brand}/modelos/${model}/anos`)
+		fetch(`https://parallelum.com.br/fipe/api/v1/${apiType}/marcas/${apiBrand}/modelos/${apiModel}/anos`)
 			.then(response => response.json())
 			.then(response => {
-				for (year of response) {
+				for (let year of response) {
 					yearSelector.innerHTML += `<option value="${year.codigo}">${year.nome}</option>`;
 				}
+			})
+	}
+}
+
+function changeYear() {
+	let option = yearSelector.options[yearSelector.selectedIndex];
+
+	apiYear = option.value;
+	updateInfos(apiYear)
+}
+
+function updateInfos(year) {
+	if (year !== '*') {
+		fetch(`https://parallelum.com.br/fipe/api/v1/${apiType}/marcas/${apiBrand}/modelos/${apiModel}/anos/${apiYear}`)
+			.then(response => response.json())
+			.then(response => {
+				console.log(response);
+				infoBrand.innerHTML = `<strong>Marca: </strong>${response.Marca}`;
+				infoModel.innerHTML = `<strong>Modelo: </strong>${response.Modelo}`;
+				infoYear.innerHTML = `<strong>Ano: </strong>${response.AnoModelo}`;
+				infoFuel.innerHTML = `<strong>Combustível: </strong>${response.Combustivel}`;
+				infoValue.innerHTML = `<strong>Valor: </strong>${response.Valor}`;
 			})
 	}
 }
